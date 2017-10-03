f21edaa67974caa279632a7c779f6ae442f6c614
Robert Sek <robert.sek@powel.no>
2017-10-03 11:11:24 +0200

    Add JsonConverter

---
+++ Azure.Table.Storage.JsonConverter/JsonPropertyConverter.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Azure.Table.Storage.JsonConverter
+{
+    internal static class JsonPropertyConverter
+    {
+        internal static bool IsSimpleType(Type type)
+        {
+            return
+                type.GetTypeInfo().IsValueType ||
+                type.GetTypeInfo().IsPrimitive ||
+                new[]
+                {
+                typeof(string),
+                typeof(String),
+                typeof(decimal),
+                typeof(Decimal),
+                typeof(DateTime),
+                typeof(DateTimeOffset),
+                typeof(TimeSpan),
+                typeof(Guid)
+                }.Contains(type) ||
+                Convert.GetTypeCode(type) != TypeCode.Object;
+        }
+
+        internal static void Serialize<TEntity>(TEntity baseEntity, IDictionary<string, EntityProperty> results, Type entityType)
+        {
+            var baseEntityType = GetEntityType(entityType);
+            var entity = Convert.ChangeType(baseEntity, baseEntityType);
+
+            foreach (var property in entityType.GetRuntimeProperties().Where(p => !IsSimpleType(p.PropertyType)))
+            {
+                var entityProperty = entityType.GetRuntimeProperty(property.Name)?.GetValue(entity);
+
+                results.Add(property.Name, new EntityProperty(JsonConvert.SerializeObject(entityProperty)));
+            }
+        }
+
+        internal static void Deserialize<TEntity>(TEntity baseEntity, IDictionary<string, EntityProperty> properties, Type entityType)
+        {
+            var baseEntityType = GetEntityType(entityType);
+            var entity = Convert.ChangeType(baseEntity, baseEntityType);
+
+            foreach (var property in entityType.GetRuntimeProperties().Where(p => !IsSimpleType(p.PropertyType)))
+            {
+                var propertyName = property.Name;
+
+                if (!properties.ContainsKey(propertyName)) continue;
+
+                var objectValue = JsonConvert.DeserializeObject(properties[propertyName].StringValue,
+                    property.PropertyType);
+                entityType.GetRuntimeProperty(property.Name)?.SetValue(entity, objectValue);
+            }
+        }
+
+        private static Type GetEntityType(Type entityType)
+        {
+            return entityType;
+        }
+    }
+}
---
+++ Azure.Table.Storage.JsonConverter/JsonTableEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+
+namespace Azure.Table.Storage.JsonConverter
+{
+    public class JsonTableEntity : TableEntity
+    {
+        [IgnoreProperty]
+        private Type EntityType { get; }
+
+        public JsonTableEntity(Type entityType)
+        {
+            EntityType = entityType;
+        }
+
+        public override void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        {
+            base.ReadEntity(properties, operationContext);
+            JsonPropertyConverter.Deserialize(this, properties, EntityType);
+        }
+
+        public override IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+        {
+            var entity = base.WriteEntity(operationContext);
+            JsonPropertyConverter.Serialize(this, entity, EntityType);
+            return entity;
+        }
+    }
+}
